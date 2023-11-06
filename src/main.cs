9a578c8f1e7b07a14410cd5ae9385cb70f989390
SOJA_Dev <rudcksdid@naver.com>
2023-11-06 13:38:47 +0000

    test again

--- src/main.cs
+++ src/main.cs
@@ -4,21 +4,7 @@ using System.Windows.Forms;
 
 class MForm : Form {
     public MForm() {
-        Text = "Simple menu";
-
-        MenuStrip ms = new MenuStrip();
-        ms.Parent = this;
         
-        ToolStripMenuItem file = new ToolStripMenuItem("&File");          
-        ToolStripMenuItem exit = new ToolStripMenuItem("&Exit", null,
-            new EventHandler(OnExit));          
-        exit.ShortcutKeys = Keys.Control | Keys.X;
-        file.DropDownItems.Add(exit);
-
-        ms.Items.Add(file);
-        MainMenuStrip = ms;
-        Size = new Size(250, 200);
-
         CenterToScreen();
     }
 
